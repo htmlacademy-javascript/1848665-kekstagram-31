de3bc8f9ec6d25dfb465fa2e977dd41f2c05b5a5
aleksy.anishchenko <aleksy.anishchenko@gmail.com>
2024-02-20 00:07:31 +0700

    Добавляет функции для тренировки

---
+++ js/functions.js
@@ -0,0 +1,16 @@
+const isValidLength = function (string, maxLength) {
+  return string.length <= maxLength;
+};
+
+isValidLength('проверяемая строка', 20);
+
+const isValidPalindrome = function (string) {
+  const formattedString = (string.replaceAll(' ', '')).toLowerCase();
+  let emptyString = '';
+  for (let i = formattedString.length - 1; i >= 0; i--) {
+    emptyString += formattedString[i];
+  }
+  return formattedString === emptyString;
+};
+
+console.log(isValidPalindrome('До вОд'));
