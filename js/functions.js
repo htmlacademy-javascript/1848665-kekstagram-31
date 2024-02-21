beefe70eea230ab55e580aa6081dbf0abe995c8d
aleksy.anishchenko <aleksy.anishchenko@gmail.com>
2024-02-22 04:19:58 +0700

    Добавляет исправленные функции

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,43 @@
-const isValidLength = function (string, maxLength) {
-  return string.length <= maxLength;
+const checkStringlength = (string = '', maxLength = 1) => string.length <= maxLength;
+
+checkStringlength();
+
+// console.log(checkStringlength('проверяемая строка', 20)); true
+// console.log(checkStringlength('проверяемая строка', 18)); true
+// console.log(checkStringlength('проверяемая строка', 10)); false
+
+const isPalindrome = (string = '') => {
+  const formattedString = string.replaceAll(' ','').toLowerCase();
+  const reversedString = formattedString.split('').reverse().join('');
+  return formattedString === reversedString;
 };
 
-isValidLength('проверяемая строка', 20);
+isPalindrome();
 
-const isValidPalindrome = function (string) {
-  const formattedString = (string.replaceAll(' ', '')).toLowerCase();
-  let emptyString = '';
-  for (let i = formattedString.length - 1; i >= 0; i--) {
-    emptyString += formattedString[i];
+// console.log(isPalindrome('топот')); true
+// console.log(isPalindrome('ДовОд')); true
+// console.log(isPalindrome('Кекс')); false
+// console.log(isPalindrome('Лёша на полке клопа нашёл ')); true
+
+const getNumbers = (string = '') => {
+  let result = '';
+  string = string.toString();
+  for (let index = 0; index <= string.length - 1; index++) {
+    const symbol = parseInt(string[index], 10);
+    if (!isNaN(symbol)) {
+      result = result + symbol;
+    }
   }
-  return formattedString === emptyString;
+  return parseInt(result, 10);
 };
 
-isValidPalindrome('До вОд');
+getNumbers();
+
+// console.log(getNumbers('2023 год')); 2023
+// console.log(getNumbers('ECMAScript 2022')); 2022
+// console.log(getNumbers('1 кефир, 0.5 батона')); 105
+// console.log(getNumbers('агент 007')); 7
+// console.log(getNumbers('а я томат')); NaN
+// console.log(getNumbers(2023)); 2023
+// console.log(getNumbers(-1)); 1
+// console.log(getNumbers(1.5)); 15
