9608efb08b842c235d9eda31f72cdaa64434f6d5
aleksy.anishchenko <aleksy.anishchenko@gmail.com>
2024-03-07 01:59:02 +0700

    Добавляет функцию по расчету времени встреч

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-import { getArrayPhotoDescriptions } from './createArrayPhotoDescriptions.js';
+import { getArrayPhotoDescriptions } from './create-array-photo-descriptions.js';
+import { checkMeetingTime } from './functions.js';
 
 // eslint-disable-next-line
 console.log(
   getArrayPhotoDescriptions()
+);
+// eslint-disable-next-line
+console.log(
+  checkMeetingTime('8:0', '10:0', '8:0', 120)
 );
